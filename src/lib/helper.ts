6b9d1731d4f77c3f8e295858518cf63b6bc562de
Jan <jmenne@uni-muenster.de>
2024-07-27 16:48:45 +0200

    feat: add trcp and prisma api

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -4,15 +4,13 @@ type OpenGraphType = {
   templateTitle?: string;
   logo?: string;
 };
-// !STARTERCONF This OG is generated from https://github.com/theodorusclarence/og
 // Please clone them and self-host if your site is going to be visited by many people.
 // Then change the url and the default logo.
 export function openGraph({
   siteName,
   templateTitle,
   description,
-  // !STARTERCONF Or, you can use my server with your own logo.
-  logo = 'https://og.<your-domain>/images/logo.jpg',
+  logo = 'https://localhost:3000/images/logo.jpg',
 }: OpenGraphType): string {
   const ogLogo = encodeURIComponent(logo);
   const ogSiteName = encodeURIComponent(siteName.trim());
@@ -21,7 +19,7 @@ export function openGraph({
     : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  return `https://og.<your-domain>/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+  return `https://localhost:3000/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
     ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
   }`;
 }
