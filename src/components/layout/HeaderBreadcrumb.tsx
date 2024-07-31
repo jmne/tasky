2fd0f1c392d6d6df9c7897efc413d1d80ed9b076
Jan <jmenne@uni-muenster.de>
2024-07-31 16:25:36 +0200

    feat: update navbar

--- src/components/layout/HeaderBreadcrumb.tsx
+++ src/components/layout/HeaderBreadcrumb.tsx
@@ -1,61 +1,69 @@
-"use client"
+'use client';
 
-import {ChevronDownIcon} from "lucide-react";
-import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { ChevronDownIcon } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import {Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbSeparator} from "@/components/ui/breadcrumb";
-import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
-import {trpc} from "@/utils/trpc";
+import { trpc } from '@/utils/trpc';
 
 export default function HeaderBreadcrumb() {
+  const path = usePathname();
+  const project_id = parseInt(path.split('/')[1]);
+  const projects = trpc.getProjects.useQuery();
+  const projectsById = trpc.getProjectById.useQuery({
+    id: project_id ? project_id : 1,
+  });
 
-    const path = usePathname()
-
-    if (path === "/") {
-        return (
-            <div className="flex w-full align-middle justify-start pl-6">
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbSeparator/>
-                        <BreadcrumbItem>
-                            Home
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </div>
-        )
-
-    } else {
-
-        const project_id = parseInt(path.split("/")[1])
-        const projectsById = trpc.getProjectById.useQuery({id: project_id})
-        const projects = trpc.getProjects.useQuery()
-
-        return (
-            <div className="flex w-full align-middle justify-start pl-6">
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbSeparator/>
-                        <BreadcrumbItem>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger className="flex items-center gap-1">
-                                    {projectsById.data?.name}
-                                    <ChevronDownIcon/>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent align="start">
-                                    {projects.data?.map((project) => (
-                                        <DropdownMenuItem key={project.id}>
-                                            <Link
-                                                href={"/" + project.id}>{project.name}</Link></DropdownMenuItem>
-                                    ))}
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </div>
-        )
-    }
+  if (path === '/') {
+    return (
+      <div className='flex w-full justify-start pl-6 align-middle'>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>Home</BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    );
+  } else {
+    return (
+      <div className='flex w-full justify-start pl-6 align-middle'>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger className='flex items-center gap-1'>
+                  {projectsById.data?.name}
+                  <ChevronDownIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='start'>
+                  {projects.data?.map((project) => (
+                    <Link href={'/' + project.id} key={project.id}>
+                      <DropdownMenuItem key={project.id}>
+                        {project.name}
+                      </DropdownMenuItem>
+                    </Link>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    );
+  }
 }
